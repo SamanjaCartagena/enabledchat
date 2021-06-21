d1282900149a75dde71a38da18825c490ad121a1
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2021-06-20 23:04:31 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE,{
+    useUnifiedTopology:true,
+    useNewUrlParser:true,
+});
+
+mongoose.connection.on('error',(err)=>{
+    console.log("Mongoose Connection ERROR: "+err.message);
+})
+
+mongoose.connection.once("open", () =>{
+    console.log("MongoDB Connected!");
+} )
+
+const app = require('./app');
+
+
+
+app.listen(8000, ()=>{
+    console.log(" Server listening on port 8000");
+})
